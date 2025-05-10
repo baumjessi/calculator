27e67d3f069dcb060d14ac22b883e7135edfc6fa
JB <198005711+baumjessi@users.noreply.github.com>
2025-05-10 18:36:20 -0500

    Add button functionality using session storage
    Add functionality to number and operator buttons by creating functions
    that store each digit and operator in session storage. Create
    a function for the equals button that calls operate() using the stored
    values as function parameters. Currently, the functions only accept
    one-digit variables for "num1" and "num2".

--- main.js
+++ main.js
@@ -32,3 +32,170 @@ function operate(num1, num2, operator) {
         console.log(divide(num1, num2));
     }
 }
+
+let one = document.getElementById("one");
+one.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "1");
+    }
+    else if (sessionStorage.getItem("num1") !== null && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "1");
+    }
+}
+);
+
+let two = document.getElementById("two");
+two.addEventListener("click", (e) => {
+   if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "2");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "2");
+    }
+}
+);
+
+let three = document.getElementById("three");
+three.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "3");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "3");
+    }
+}
+);
+
+let four = document.getElementById("four");
+four.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "4");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "4");
+    }
+}
+);
+
+let five = document.getElementById("five");
+five.addEventListener("click", (e) => {
+ if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "5");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "5");
+    }
+}
+);
+
+let six = document.getElementById("six");
+six.addEventListener("click", (e) => {
+   if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "6");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "6");
+    }
+}
+);
+
+let seven = document.getElementById("seven");
+seven.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "7");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "7");
+    }
+}
+);
+
+let eight = document.getElementById("eight");
+eight.addEventListener("click", (e) => {
+  if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "8");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "8");
+    }
+}
+);
+
+let nine = document.getElementById("nine");
+nine.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "9");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "9");
+    }
+}
+);
+
+let zero = document.getElementById("zero");
+zero.addEventListener("click", (e) => {
+    if (sessionStorage.getItem("num1")===null) {
+        sessionStorage.setItem("num1", "0");
+    }
+    else if (sessionStorage.getItem("num1") !== null 
+    && sessionStorage.getItem("num2") == null) {
+        sessionStorage.setItem("num2", "0");
+    }
+}
+);
+
+let plus = document.getElementById("plus");
+plus.addEventListener("click", (e) => {
+    sessionStorage.setItem("operator", "+");
+    console.log(sessionStorage.getItem("operator"));
+}
+);
+
+let minus = document.getElementById("minus");
+minus.addEventListener("click", (e) => {
+    sessionStorage.setItem("operator", "-");
+    console.log(sessionStorage.getItem("operator"));
+}
+);
+
+let times = document.getElementById("times");
+times.addEventListener("click", (e) => {
+    sessionStorage.setItem("operator", "*");
+    console.log(sessionStorage.getItem("operator"));
+}
+);
+
+let divideSym = document.getElementById("divide");
+divideSym.addEventListener("click", (e) => {
+    sessionStorage.setItem("operator", "/");
+    console.log(sessionStorage.getItem("operator"));
+}
+);
+
+let clear = document.getElementById("clear");
+clear.addEventListener("click", (e) => {
+    sessionStorage.removeItem("num1");
+    sessionStorage.removeItem("num2");
+    sessionStorage.removeItem("operator");
+}
+);
+
+let equal = document.getElementById("equal");
+equal.addEventListener("click", (e) => {
+    let num1 = Number(sessionStorage.getItem("num1"));
+    let num2 = Number(sessionStorage.getItem("num2"));
+    let operator = sessionStorage.getItem("operator");
+    console.log(operate(num1, num2, operator));
+    sessionStorage.removeItem("num1");
+    sessionStorage.removeItem("num2");
+    sessionStorage.removeItem("operator");
+}
+);
